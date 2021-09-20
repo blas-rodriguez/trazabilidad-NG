ff2c110723627d80b98e59b9a520a35000e64eb9
blas-rodriguez <blasrodriguez934@gmail.com>
2021-09-20 10:01:37 -0300

    add url

--- trazabilidad-angular/src/app/services/global.ts
+++ trazabilidad-angular/src/app/services/global.ts
@@ -1,3 +1,5 @@
 export var global = {
-    url: 'http://trazabilidad.com.devel/api/'
+    // url: 'http://trazabilidad.com.devel/api/'
+    // url: 'http://127.0.0.1:8000/api/'
+    url: 'http://bluestar.hol.es/api-rest-laravel/api/'
 }
